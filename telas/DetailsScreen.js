e32c23c89173df328df3d04e815658da22ec9e94
Bruna Morais Menezes <141691997+BrunaMoraisMenezesDSM@users.noreply.github.com>
2024-05-04 01:03:31 +0000

    Finalizado navegação

---
+++ telas/DetailsScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+function DetailsScreen(navigation) {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button title="Voltar" onPress={() => navigation.navigation.goBack()}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+  }
+});
+
+export default DetailsScreen;
---
+++ telas/HomeScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, Button } from 'react-native';
+import { StyleSheet } from 'react-native';
+ 
+function HomeScreen({ navigation }) {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <Button title="Ir para detalhes" onPress={() => navigation.navigate('Detalhes')}
+        />
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+  }
+});
+ 
+export default HomeScreen;
